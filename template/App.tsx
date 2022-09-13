bf08660a0bfc7f097472a73a2aa28812cc08e621
Kirill Babich <kirill.babich@mail.ru>
2022-09-13 09:42:26 +0200

    Add template

---
+++ template/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Here will be your app</Text>
+    </View>
+  );
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
